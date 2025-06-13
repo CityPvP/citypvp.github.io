823823931d28471b59f8fc88fb89107e42e726d0
⸨CIƬYPvP.DΣ⸩ <158457625+CityPvP@users.noreply.github.com>
2025-06-13 18:47:57 +0200

    Add files via upload

--- js/overlay.js
+++ js/overlay.js
@@ -3,23 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const mainContent = document.getElementById('main-content');
     const audio = document.getElementById('background-music');
 
-    // Stelle sicher, dass die Musik initial pausiert ist
     audio.pause();
     audio.currentTime = 0; // Zurücksetzen auf Anfang
 
-    // Event-Listener für Klick auf das Overlay
     overlay.addEventListener('click', function () {
-        // Verstecke das Overlay
         overlay.style.display = 'none';
-        // Zeige den Hauptinhalt
         mainContent.classList.remove('hidden');
-        // Starte die Musik
         audio.play().catch(function (error) {
             console.error('Fehler beim Abspielen der Musik:', error);
         });
     });
 
-    // Verhindere, dass der Klick auf das Overlay die Seite scrollt
     overlay.addEventListener('click', function (e) {
         e.preventDefault();
     });
